9d287c72f4e58ceefe0ab26cab89233a30358ca3
baoqinliao <296013072@qq.com>
2023-02-23 20:45:59 +0800

    2023年第十三次提交 2023-02-23-8:45

--- zhuoDjangoAdmin/newzhuoadmin/webpage/middleware/auth.py
+++ zhuoDjangoAdmin/newzhuoadmin/webpage/middleware/auth.py
@@ -7,7 +7,7 @@ class AuthMiddleware(MiddlewareMixin):
     def process_request(self,request):
         #0.排除不需要登录就访问的页面
         #request.path_info #获取当前用户请求的url /login/
-        if request.path_info in ["/login/","/newlogin/","/neworders/","/test/","/newtest/"]:
+        if request.path_info in ["/login/","/newlogin/","/neworders/","/test/","/newtest/","/getMusic/","/getChan/"]:
             return
         #1.读取当前访问的用户的session信息,如果能读到,说明已登录过就可以继续向后走,如果没有说明没登录
         info=request.session.get("info")
